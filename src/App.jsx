cb6cb011dacaedbee395a8fca5f7ecdaad820520
mohamed Nowfan <mohamednowfan30@gmail.com>
2025-03-21 05:58:54 +0530

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+// import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbars from './Components/Navbars';
 import Footer from './Components/Footer';
